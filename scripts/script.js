5895717ecbe584b96208170569b0dd1f5272cc4e
LeraZip <83587616+LeraZip@users.noreply.github.com>
2021-07-25 20:06:03 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+function setColor(e) {
+  var target = e.target,
+      status = e.target.classList.contains('active');
+  
+  e.target.classList.add(status ? 'inactive' : 'active');
+  e.target.classList.remove(status ? 'active' : 'inactive');
+}
+
+function setColor2(e) {
+  var target = e.target,
+      status = e.target.classList.contains('active2');
+  
+  e.target.classList.add(status ? 'inactive2' : 'active2');
+  e.target.classList.remove(status ? 'active2' : 'inactive2');
+}
+
+
+function setColor3(e) {
+  var target = e.target,
+      status = e.target.classList.contains('active3');
+  
+  e.target.classList.add(status ? 'inactive3' : 'active3');
+  e.target.classList.remove(status ? 'active3' : 'inactive3');
+}
+
+function setColor4(e) {
+  var target = e.target,
+      status = e.target.classList.contains('active4');
+  
+  e.target.classList.add(status ? 'inactive4' : 'active4');
+  e.target.classList.remove(status ? 'active4' : 'inactive4');
+}
+
+function setColor5(e) {
+  var target = e.target,
+      status = e.target.classList.contains('active5');
+  
+  e.target.classList.add(status ? 'inactive5' : 'active5');
+  e.target.classList.remove(status ? 'active5' : 'inactive5');
+}
+
+function setColor6(e) {
+  var target = e.target,
+      status = e.target.classList.contains('active6');
+  
+  e.target.classList.add(status ? 'inactive6' : 'active6');
+  e.target.classList.remove(status ? 'active6' : 'inactive6');
+}
